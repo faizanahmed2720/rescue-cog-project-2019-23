a0f2d78fda22401610fd5310c44a21738f034d07
Faizan Ahmed <rannar2720@gmail.com>
2023-05-01 12:22:22 +0500

    First Aid Guide Diseases

--- lib/View/First Aid Guide/disease_1.dart
+++ lib/View/First Aid Guide/disease_1.dart
@@ -1,15 +1,57 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+// ignore: file_names
 
-class Disease1 extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:untitled/src/Constants/colors.dart';
+
+class Disease1 extends StatefulWidget {
   const Disease1({super.key});
 
   @override
+  State<Disease1> createState() => _Disease1State();
+}
+
+class _Disease1State extends State<Disease1> {
+  @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SafeArea(
       child: Scaffold(
-        body: Center(child: Text("COVID-19")),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 150,
+                alignment: Alignment.center,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                  color: primaryColor,
+                ),
+                child: const Center(
+                  child: Text(
+                    "     COVID-19 \n PRECAUTIONS",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child:
+                        Image.asset("assets/Images/First Aid Guide/Doctor.gif"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
